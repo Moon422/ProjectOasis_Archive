1b53348fdbfadd13335948d708e46e8821360460
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-01-08 10:59:25 +0600

    configured mysql db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auth } from './auth/auth.entity';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'oasisdb',
+      username: 'oasisuser',
+      password: 'H@2ami$zz',
+      synchronize: true,
+      entities: [Auth],
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
